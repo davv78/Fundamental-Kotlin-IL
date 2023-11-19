aaf0b8caeac8ee7ca39c3aaf6e056611dbe58791
davv03 <nakanonino577@gmail.com>
2023-11-19 17:42:45 +0700

    bismillah

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+val kotlin_version = "1.8.0" // Ganti dengan versi Kotlin yang diinginkan
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
